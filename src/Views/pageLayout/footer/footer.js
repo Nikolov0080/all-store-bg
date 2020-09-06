9b827a728a8d0db3f8bba32d5c068505c7c3ea45
Nikolov0080 <apofis12343@gmail.com>
2020-09-07 01:41:19 +0300

    changed footer

--- src/Views/pageLayout/footer/footer.js
+++ src/Views/pageLayout/footer/footer.js
@@ -6,16 +6,11 @@ const Footer = (props) => {
     return (
         <div >
             <Card className={style.footer}>
-                <Card.Header>Featured</Card.Header>
-                <Card.Body>
-                    {/* <Card.Title>Special title treatment</Card.Title> */}
-                    <Card.Text>
-                        With supporting text below as a natural lead-in to additional content.
-                    </Card.Text>
-                </Card.Body>
+                <Card.Header className="text-center">Featured</Card.Header>
+              
             </Card>
         </div>
     )
 }
 
-export default Footer
+export default Footer;
