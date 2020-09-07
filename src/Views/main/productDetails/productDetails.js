6e46e8eb184505944d527d5d121e97b69d0d10c7
Nikolov0080 <apofis12343@gmail.com>
2020-09-07 15:25:47 +0300

    created product details

--- src/Views/main/productDetails/productDetails.js
+++ src/Views/main/productDetails/productDetails.js
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import firebase from 'firebase';
+import { Card } from 'react-bootstrap';
+import style from './details.module.css';
 
 const ProductDetails = (props) => {
+
+    console.log(props.location.state)
+    const productId = props.location.state.productId;
+    const [currentProduct, setCurrentProduct] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        firebase.database().ref('users/').once('value', (snapshot) => {
+
+            const products = Object.values(snapshot.val())
+
+            const allProds = products.reduce((acc, cVal) => {
+
+                Object.assign(acc, cVal)
+
+                return acc
+            }, {})
+
+            setCurrentProduct(allProds[productId])
+        })
+    }, [productId])
+
+
+    if (currentProduct === null) {
+        return (
+            <h1>Loading...</h1>
+        )
+    }
+
     return (
         <div>
-            Product details
+            <Card className={style.box} >
+                <Card.Header><h4>{currentProduct.title} </h4></Card.Header>
+                <Card.Img className={style.photo} src={props.location.state.imageUrl} />
+                <div className="text-center">
+                    <Card.Body>
+                        <Card.Header><h3>Description</h3>
+                            <br />
+                            {currentProduct.description}
+                        </Card.Header>
+                    </Card.Body>
+                </div>
+            </Card>
         </div>
     )
 }
--- src/Views/main/productList/productList.js
+++ src/Views/main/productList/productList.js
@@ -1,16 +1,20 @@
-import React, {  useState, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import firebase from 'firebase';
 import { Card, Button, Row, Col } from 'react-bootstrap';
 import { Link } from 'react-router-dom';
 import style from './productList.module.css';
+import ErrorBoundary from '../../../errorBoundaries/errorBoundary';
 
 const ProductList = (props) => {
 
     const [products, setProducts] = useState([]);
     const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(false);
+
     const path = `/products/`;
 
     /// FIREBASE DATA FETCH...
+
     const getImagePromise = (id) => {
         return firebase.storage().ref('images/' + id).getDownloadURL().then((url) => {
             return url
@@ -18,75 +22,75 @@ const ProductList = (props) => {
     }
 
     useEffect(() => {
-        firebase.database().ref('users/').once('value', async (snapshot) => {// get all users and userProducts in DB
-            await snapshot.forEach(item => {
+        try {
 
-                const prods = Object.values(item.val())
-                prods.forEach((productData) => {
+            firebase.database().ref('users').once('value', async (snapshot) => {// get all users and userProducts in DB
+                await snapshot.forEach(item => {
 
-                    getImagePromise(productData.imageId).then((imageResponse) => {
+                    const prods = Object.values(item.val())
+                    prods.forEach((productData) => {
 
-                        setProducts(oldArray => [...oldArray, { ...productData, imageUrl: imageResponse }]);
+                        getImagePromise(productData.imageId).then((imageResponse) => {
+
+                            setProducts(oldArray => [...oldArray, { ...productData, imageUrl: imageResponse }]);
+                        })
                     })
                 })
-            })
-        }).then(() => {
-         
+            }).then(() => {
                 setLoading(false)
-       
-        })
+            })
+        } catch (error) {
+            setError(error)
+        }
 
-    }, [])
+
+    }, [error])
 
     /// FIREBASE DATA FETCH...
 
     if (loading) {
         return (
             <div>
-                <div>
-                    Product list
-                <Row >
-                        {products.map(({ imageUrl, title, price, description, creator }, index) => {
-                            return (
-                                <Col md key={index}>
-                                    <Card className={style.size}>
-                                     <Card.image />
-                                       
-                                    </Card>
-                                </Col>
-                            )
-                        })}
-                    </Row>
+                <div className="text-center">
+                    <h1>Loading...</h1>
                 </div>
             </div>
         )
     } else {
         return (
             <div>
-                Product list
-                <Row >
-                    {products.map(({ imageUrl, title, price, description, creator }, index) => {
-                        return (
-                            
-                            <Col md key={index}>
-                                <Card className={style.size}>
-                                    <Card.Img className={style.sizeImg} variant="top" src={imageUrl} />
-                                    <Card.Body>
-                                        <Card.Title>{title}</Card.Title>
+                <ErrorBoundary message='Server do not respond , please try again later'>
+                    {error !== false ? new Error() : ''}
+                    <Row >
+                        {products.map(({ imageUrl, title, price, description, creator,imageId }, index) => {
+                            return (
 
-                                        <Card.Footer className="text-center">
-                                            <Card.Text>Price: {price}   </Card.Text>
-                                            <Card.Text>Posted by : [ {creator} ]</Card.Text>
-                                        </Card.Footer>
-                                        <Button variant="success" style={{width:"100%"}}>
-                                            <Link to={path + creator.toLowerCase()}>View details</Link>
-                                        </Button>
-                                    </Card.Body>
-                                </Card>
-                            </Col>
-                        )
-                    })}
-                </Row>
+                                <Col md key={index}>
+                                    <Card className={style.size}>
+                                        <Card.Img className={style.sizeImg} variant="top" src={imageUrl} />
+                                        <Card.Body>
+                                            <Card.Title>{title}</Card.Title>
+
+                                            <Card.Footer className="text-center">
+                                                <Card.Text>Price: {price}   </Card.Text>
+                                                <Card.Text>Posted by : [ {creator} ]</Card.Text>
+                                            </Card.Footer>
+                                            <Link to={{
+                                                pathname: path + creator.toLowerCase(),
+                                                state: { productId:imageId,imageUrl }
+                                            }}>
+
+                                                <Button variant="success" style={{ width: "100%" }}>Details</Button>
+
+                                            </Link>
+                                        </Card.Body>
+                                    </Card>
+                                </Col>
+                            )
+                        })}
+                    </Row>
+                </ErrorBoundary>
+
             </div>
         )
     }
