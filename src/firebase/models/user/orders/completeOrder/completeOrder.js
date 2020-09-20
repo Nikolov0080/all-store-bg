d8933272d2ebb2d7cb522324a16852fa4fcedf16
Nikolov0080 <apofis12343@gmail.com>
2020-09-20 21:49:22 +0300

    created: orders history in DB

--- src/firebase/models/user/orders/completeOrder/completeOrder.js
+++ src/firebase/models/user/orders/completeOrder/completeOrder.js
@@ -1,21 +1,31 @@
 import firebase from 'firebase';
+import moment from 'moment';
 
 const orderDataRef = firebase.database().ref('users');
 const imageRef = firebase.storage().ref('images');
-const completeOrder = (imageId,userId,orderId) => {
-console.log(imageId);
-console.log(userId);
-console.log(orderId);
+const completeOrder = (imageId, userId, orderId) => {
+    console.log(imageId);
+    console.log(userId);
+    console.log(orderId);
 
-const a = orderDataRef.child(userId+"/purchases/" + orderId).remove((err)=>{
-    console.log(err);
-}).catch((e)=>console.log(e));
+    return orderDataRef.child(userId + "/purchases/" + orderId).once("value", (snapshot) => {
+        firebase.database()
+            .ref("Completed Orders --History/" + moment().format('MMMM Do YYYY, h:mm:ss a'))
+            .set(snapshot.val())
+    }).then(() => {
 
-const b = imageRef.child(imageId).delete()
+        const a = orderDataRef.child(userId + "/purchases/" + orderId).remove((err) => {
+            console.log(err);
+        }).catch((e) => console.log(e));
 
-return Promise.all([a,b]).then((response)=>{
-    console.log(response)
-    return response;
-})}
+        const b = imageRef.child(imageId).delete()
+
+        return Promise.all([a, b]).then((response) => {
+            console.log(response)
+            return response;
+        })
+    })
+
+}
 
 export default completeOrder;
