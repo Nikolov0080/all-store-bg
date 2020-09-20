345efa83d6f7cf29539a64750b7edd1e7c021886
Nikolov0080 <apofis12343@gmail.com>
2020-09-20 15:19:04 +0300

    finish pagination style

--- src/Views/components/pagination/pagination.js
+++ src/Views/components/pagination/pagination.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
-import { Button, Form } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
 import ReactPaginate from 'react-paginate';
-
+import style from './style.module.css'
 const Pagination = ({ products, setCurrentSelect, perPage, setPerPage }) => {
 
     const [pageCount, setPageCount] = useState(0);
@@ -21,14 +21,11 @@ const Pagination = ({ products, setCurrentSelect, perPage, setPerPage }) => {
     useEffect(() => {
         setPageCount(Math.ceil(products.length / perPage));
     }, [products, perPage, setCurrentSelect])
-    // console.log(perPage)
-
+  
     return (
         <div className='text-center mt-5'>
 
-            <ReactPaginate 
-                previousLabel={'previous'}
-                nextLabel={'next'}
+            <ReactPaginate
                 breakLabel={'...'}
                 breakClassName={'break-me'}
                 pageCount={pageCount} //this.state.pageCount
@@ -36,18 +33,20 @@ const Pagination = ({ products, setCurrentSelect, perPage, setPerPage }) => {
                 pageRangeDisplayed={5}
                 onPageChange={handlePageClick} //
                 containerClassName={'pagination'}
-                subContainerClassName={'pages-pagination'}
+                subContainerClassName={'pages-item'}
                 activeClassName={'active'}
             />
+            <div className={style.options}>
+                <Form.Label>Products per page</Form.Label>
+                <Form.Control
+                    onChange={(e) => changePerPage(e)}
+                    as="select">
+                    <option >5</option>
+                    <option>10</option>
+                    <option>20</option>
+                </Form.Control>
+            </div>
 
-            <Form.Label>Select Condition</Form.Label>
-            <Form.Control
-                onChange={(e) => changePerPage(e)}
-                as="select">
-                <option>5</option>
-                <option>10</option>
-                <option>20</option>
-            </Form.Control>
 
 
         </div>
